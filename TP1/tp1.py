923cef7a0582e76dfa142cc2ef8021bf09d1e7aa
Lucas <88lucas88@gmail.com>
2018-09-12 12:00:04 -0300

    punto a y b andando

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -2,9 +2,12 @@ import re
 import sys
 
 #se ejecuta python tp1.py
+a = re.compile(r':\d+s/[a-z]*/[a-z]*/g')
+b = re.compile(r':%s/[a-z]*/[a-z]*/g')
+c = re.compile(r':\d+\,\d+s/[a-z]*/[a-z]*/g')
 
 print "Lista de argumentos: ", sys.argv
-
+patron_init = sys.argv[1]
 #VER DE INGRESAR OTRO PARAMETRO PARA LA LINEA ACTUAL O TODAS LAS LINEAS
 
 
@@ -12,12 +15,52 @@ val = re.compile(r':(\d+|%|\d+\,\d+)s/[a-z]*/[a-z]*/g')
 
 #print(sys.argv[1])
 
-if val.search(sys.argv[1]) != None:
+if val.search(patron_init) != None:
     print ("Expresion regular VALIDA")
 else:
     print ("Expresion INVALIDA")
     sys.exit()
 
+archivo = open('/home/ub18/Documentos/2018/FTI/FTI/TP1/archivo.txt','r')
+
+#lineas = archivo.readlines()[2]
+#print(lineas)
+#texto = archivo.read()
+
+str_patron1 = re.split('/', patron_init)[1]
+str_patron2 = re.split('/', patron_init)[2]
+
+patron1 = re.compile(str_patron1)
+
+if a.match(patron_init):
+    print('patron ingresado A')
+    num_linea = int(re.split(r'(:|s)',patron_init)[2])
+    lineas = archivo.readlines()
+    try:
+        lineas[num_linea] = patron1.sub(str_patron2,lineas[num_linea])
+        output = open("output.txt","w")
+        output.writelines(lineas)
+        print (lineas)
+    except IndexError:
+        print('linea inexistente')
+elif b.match(patron_init):
+    print('patron ingresado B\n')
+    print('TEXTO ORIGINAL\n')
+    texto = archivo.read()
+    print(texto)
+    print('\n')
+    texto = patron1.sub(str_patron2,texto)
+    print(texto)
+else:
+    print('patron ingresado C')
+
+#print(patron_init)
+#print(re.split(r'(:|s)',patron_init)[2])
+
+#print( re.split('s', sys.argv[1]))
+
+
+"""
 palabras = re.split('/', sys.argv[1])
 p1 = palabras[1]
 p2 = palabras[2]
@@ -33,7 +76,6 @@ texto = val.sub(p2,texto)
 
 print(texto)
 
-"""
 archivo = open('/home/ub18/Documentos/2018/FTI/FTI/TP1/archivo.txt','r')
 linea = archivo.readlines()
 try:
@@ -41,12 +83,11 @@ try:
 except IndexError:
     print('linea inexistente')
 
-"""
 
 ##VALIDAR LA EXPRESION
 
 
-"""
+
 cadena = raw_input('Introduce una cadena de texto: ')
 print 'La cadena que ingreso es:\n',cadena
 valor1 = re.compile(r'foo')
